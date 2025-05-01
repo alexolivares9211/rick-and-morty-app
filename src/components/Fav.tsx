1f8f5acfed05715d6ee3299d65d654676b373adf
alexolivares9211 <alejandrolivares9211@gmail.com>
2025-05-01 16:06:47 -0600

    config and background

---
+++ src/components/Fav.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Fav = () => {
+  return <div>fav</div>;
+};
+
+export default Fav;
---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: { fontFamily: 'Roboto' },
+  cssVariables: true,
+});
+
+export default theme;
